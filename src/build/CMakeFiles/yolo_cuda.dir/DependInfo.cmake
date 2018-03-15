b25bfaa947513a408584a208fc10d87a2b99d1ed
exist_disappear <exist_disappear@sina.com>
2018-03-16 00:10:01 +0800

    opencv34 used with bugs

---
+++ src/build/CMakeFiles/yolo_cuda.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/startank/selfgit/darknet/src/activation_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/activation_layer.c.o"
+  "/home/startank/selfgit/darknet/src/activations.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/activations.c.o"
+  "/home/startank/selfgit/darknet/src/avgpool_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/avgpool_layer.c.o"
+  "/home/startank/selfgit/darknet/src/batchnorm_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/batchnorm_layer.c.o"
+  "/home/startank/selfgit/darknet/src/blas.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/blas.c.o"
+  "/home/startank/selfgit/darknet/src/box.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/box.c.o"
+  "/home/startank/selfgit/darknet/src/col2im.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/col2im.c.o"
+  "/home/startank/selfgit/darknet/src/connected_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/connected_layer.c.o"
+  "/home/startank/selfgit/darknet/src/convolutional_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/convolutional_layer.c.o"
+  "/home/startank/selfgit/darknet/src/cost_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/cost_layer.c.o"
+  "/home/startank/selfgit/darknet/src/crnn_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/crnn_layer.c.o"
+  "/home/startank/selfgit/darknet/src/crop_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/crop_layer.c.o"
+  "/home/startank/selfgit/darknet/src/cuda.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/cuda.c.o"
+  "/home/startank/selfgit/darknet/src/data.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/data.c.o"
+  "/home/startank/selfgit/darknet/src/deconvolutional_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/deconvolutional_layer.c.o"
+  "/home/startank/selfgit/darknet/src/detection_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/detection_layer.c.o"
+  "/home/startank/selfgit/darknet/src/dropout_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/dropout_layer.c.o"
+  "/home/startank/selfgit/darknet/src/gemm.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/gemm.c.o"
+  "/home/startank/selfgit/darknet/src/gru_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/gru_layer.c.o"
+  "/home/startank/selfgit/darknet/src/im2col.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/im2col.c.o"
+  "/home/startank/selfgit/darknet/src/image.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/image.c.o"
+  "/home/startank/selfgit/darknet/src/layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/layer.c.o"
+  "/home/startank/selfgit/darknet/src/list.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/list.c.o"
+  "/home/startank/selfgit/darknet/src/local_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/local_layer.c.o"
+  "/home/startank/selfgit/darknet/src/lstm_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/lstm_layer.c.o"
+  "/home/startank/selfgit/darknet/src/matrix.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/matrix.c.o"
+  "/home/startank/selfgit/darknet/src/maxpool_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/maxpool_layer.c.o"
+  "/home/startank/selfgit/darknet/src/network.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/network.c.o"
+  "/home/startank/selfgit/darknet/src/normalization_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/normalization_layer.c.o"
+  "/home/startank/selfgit/darknet/src/option_list.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/option_list.c.o"
+  "/home/startank/selfgit/darknet/src/parser.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/parser.c.o"
+  "/home/startank/selfgit/darknet/src/region_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/region_layer.c.o"
+  "/home/startank/selfgit/darknet/src/reorg_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/reorg_layer.c.o"
+  "/home/startank/selfgit/darknet/src/rnn_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/rnn_layer.c.o"
+  "/home/startank/selfgit/darknet/src/route_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/route_layer.c.o"
+  "/home/startank/selfgit/darknet/src/shortcut_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/shortcut_layer.c.o"
+  "/home/startank/selfgit/darknet/src/softmax_layer.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/softmax_layer.c.o"
+  "/home/startank/selfgit/darknet/src/tree.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/tree.c.o"
+  "/home/startank/selfgit/darknet/src/utils.c" "/home/startank/selfgit/darknet/src/build/CMakeFiles/yolo_cuda.dir/utils.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GPU"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/local/cuda-8.0/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
